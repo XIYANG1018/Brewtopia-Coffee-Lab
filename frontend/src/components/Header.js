c7dd48457d6f9076a4d72606f7ed239017bf2e0f
XIYANG1018 <pkuyangxi@163.com>
2024-07-01 10:38:38 -0500

    update cart items display

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,12 +1,18 @@
 import React from 'react'
-import { Navbar, Nav, Container } from 'react-bootstrap';
+import { Badge, Navbar, Nav, Container } from 'react-bootstrap';
 import { FaShoppingCart, FaUser } from 'react-icons/fa'
 import logo from "../assets/logo.png"
 import styles from "./Components.modules.css"
 import { LinkContainer } from 'react-router-bootstrap';
+import { useSelector } from 'react-redux';
 
 
 const Header = () => {
+
+  // destructure what we need from the state by using useSelector
+  const { cartItems } = useSelector(state => state.cart);
+  console.log(cartItems);
+  
   return (
     <header>
       <Navbar bg="transparent" variant='dark' expand="md" collapseOnSelect>
@@ -22,7 +28,14 @@ const Header = () => {
                 <Nav className='ms-auto'>
                   <LinkContainer to='/cart'>
                     <Nav.Link className='header-text '>
-                      <FaShoppingCart className='icon'/>Cart</Nav.Link>
+                      <FaShoppingCart className='icon'/>Cart {
+                        cartItems.length > 0 && (
+                          <Badge pill bg='dark' style={{marginLeft: '5px'}}>
+                            { cartItems.reduce((acc, item) => acc + item.qty, 0 )}
+                          </Badge>
+                        )
+                      
+                      }</Nav.Link>
                   </LinkContainer>
                   <LinkContainer to='/login'>
                     <Nav.Link className='header-text '><FaUser className='icon'/>Sign In</Nav.Link>
